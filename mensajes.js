14bd9c9f72ffc52c8659f7aa4a9d480cc77d372b
Manases Lovera <manaseslovera07@gmail.com>
2022-06-01 18:54:25 -0400

    Menu realizado

--- mensajes.js
+++ mensajes.js
@@ -4,9 +4,17 @@ const mostrarMenu = () => {
 
     console.clear()
 
-    console.log("==========================")
-    console.log("  Seleccione una opcion")
-    console.log("==========================")
+    console.log("==========================".green)
+    console.log("  Seleccione una opcion".green)
+    console.log("==========================\n".green)
+
+    console.log(`1. Crear tarea`)
+    console.log(`2. Mostrar tareas`)
+    console.log(`3. Mostrar tareas completadas`)
+    console.log(`4. Mostrar tareas pendientes`)
+    console.log(`5. Completar tarea(s)`)
+    console.log(`6. Borrar tarea`)
+    console.log(`0. Salir`)
 }
 
 module.exports = {
